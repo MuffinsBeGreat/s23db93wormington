688fd167001d0edf11a2ceed5a78e59347566cf2
Casey Wormington <54279844+MuffinsBeGreat@users.noreply.github.com>
2023-11-08 15:34:21 -0600

    Added code to add elements to database

--- controllers/microphone.js
+++ controllers/microphone.js
@@ -18,8 +18,26 @@ exports.microphone_detail = function(req, res) {
 };
 
 // Handle microphone create on post
-exports.microphone_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Microphone create POST');
+exports.microphone_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Microphone();
+
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.microphone_name = req.body.microphone_name;
+    document.cost = req.body.cost;
+    document.easyToUse = req.body.easyToUse;
+
+    try {
+        let result = await document.save()
+        res.send(result);
+    }
+    catch(err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 
 // Handle Microphone delete form on DELETE
