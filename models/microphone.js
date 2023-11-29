34ee5c246e66327ede15c9433c4a5771d7f5ea2c
Casey Wormington <54279844+MuffinsBeGreat@users.noreply.github.com>
2023-11-29 15:58:58 -0600

    Added bonus authentication to create and delete views, attempt at fixing npm start bug within render

--- models/microphone.js
+++ models/microphone.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const microphoneSchema = mongoose.Schema({
-    microphone_name: String,
+    microphone_name: {type: String, lowercase: true},
     cost: { type: Number, min: 0, max: 1500 },
     easyToUse: Boolean
 })
--- routes/microphone.js
+++ routes/microphone.js
@@ -14,7 +14,7 @@ const secured = (req, res, next) => {
 /* GET home page. */
 router.get('/', microphone_controllers.microphone_view_all_Page);
 router.get('/detail', microphone_controllers.microphone_view_one_Page);
-router.get('/create', microphone_controllers.microphone_create_Page);
+router.get('/create', secured, microphone_controllers.microphone_create_Page);
 router.get('/update', secured, microphone_controllers.microphone_update_Page);
-router.get('/delete', microphone_controllers.microphone_delete_Page);
+router.get('/delete', secured, microphone_controllers.microphone_delete_Page);
 module.exports = router;
