ede6c97651eef0aeeb970f0e143611d33b07bdcf
Casey Wormington <54279844+MuffinsBeGreat@users.noreply.github.com>
2023-11-29 15:26:56 -0600

    Added authentication check for updating the collection

--- routes/microphone.js
+++ routes/microphone.js
@@ -1,11 +1,20 @@
 var express = require('express');
 const microphone_controllers = require('../controllers/microphone');
+const { rawListeners } = require('../models/account');
 var router = express.Router();
+
+// Function to check if we have an authorized user and continue or redirect to login
+const secured = (req, res, next) => {
+    if(req.user) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 /* GET home page. */
 router.get('/', microphone_controllers.microphone_view_all_Page);
 router.get('/detail', microphone_controllers.microphone_view_one_Page);
 router.get('/create', microphone_controllers.microphone_create_Page);
-router.get('/update', microphone_controllers.microphone_update_Page);
+router.get('/update', secured, microphone_controllers.microphone_update_Page);
 router.get('/delete', microphone_controllers.microphone_delete_Page);
 module.exports = router;
